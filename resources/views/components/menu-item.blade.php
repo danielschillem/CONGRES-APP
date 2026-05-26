fe7d562ca1b5448936ab71c819f44e414a49b333
Daniel Schillem (Baba Yaga) <64925634+danielschillem@users.noreply.github.com>
2026-05-26 14:20:25 +0000

    Consolidate monorepo structure

--- resources/views/components/menu-item.blade.php
+++
@@ -1,6 +0,0 @@
-@props(['icon', 'label', 'active' => false])
-
-<div class="flex items-center space-x-3 mb-4 p-2 rounded {{ $active ? 'bg-indigo-700' : 'hover:bg-indigo-700' }}">
-    <x-icon :name="$icon" />
-    <span>{{ $label }}</span>
-</div>
